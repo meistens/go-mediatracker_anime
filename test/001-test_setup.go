6b5e30d996247f9ef6095db3b59a0c8e63a63b63
Alto <102149853+meistens@users.noreply.github.com>
2025-08-20 19:02:00 +0100

    tests initial passes

--- test/001-test_setup.go
+++ test/001-test_setup.go
@@ -4,10 +4,16 @@ import (
 	"fmt"
 
 	"gioui.org/app"
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
 	// test installed deps and Gio
 	fmt.Println("Deps installs correct!")
 	fmt.Printf("Gio app package: %T\n", &app.Window{})
+
+	logger := logrus.New()
+	logger.Info("logrus works...")
+
+	fmt.Println("all works as it should")
 }
